1181d373ca431edc71ed0eec5f4e3c13b4fce596
i180394 <mortuarct@gmail.com>
2019-09-19 17:11:18 +0200

    "mise a joour"

--- Connexion.php
+++ Connexion.php
@@ -1,21 +1,6 @@
 
 <?php
-/*
-$user = "i180367";
-$pass = "vmb89zn";
-$host = "http://la-myweb.univ-lemans.fr/phpMyAdmin/"; (remplacer par Localhost après déploiement, port 3308)
-$base = "INF2_i180367";
-try {
-    $dbh = new PDO("mysql:host=$host;dbname=$base", $user, $pass);
-    foreach($dbh->query('SELECT * from USER') as $row) {
-        print_r($row);
-    }
-    $dbh = null;
-} catch (PDOException $e) {
-    print "Erreur !: " . $e->getMessage() . "<br/>";
-    die();
-}
-*/
+
 if (isset($_POST["config_user"])  && isset($_POST["config_pass"] )) {
     if ($_POST["config_user"] == "admin" && $_POST["config_pass"] == "totoro") {
         header("Location: Connecte.php");

dd64f1ca8aab915931a5066ba269740fb1151831
Rafael Doneau <rafa72don@gmail.com>
2019-09-23 10:54:29 +0200

    Listes Récaps

---
+++ listeRecap.php
@@ -0,0 +1,32 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <link type="text/css" rel="stylesheet" href="cssListeRecap.css">
+    <meta charset="UTF-8">
+    <title>Recap</title>
+</head>
+<body>
+<div class="grid-container">
+    <div class="categorie">
+        <?php
+        echo("Films à voir ou à revoir"."<br/>")
+        ?>
+    </div>
+    <div class="ajout">
+        <?php
+        echo("Ajout d'une fiche"."<br/>")
+        ?>
+    </div>
+    <div class="fiches">
+        <?php
+        echo("Fiches"."<br/>")
+        ?>
+    </div>
+    <div class="footer"></div>
+</div>
+</body>
+</html>

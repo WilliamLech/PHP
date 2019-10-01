52b3d6ba2cc12d4b324f50993a43dd7e9ee674d7
william <williamlechat53@gmail.com>
2019-10-01 12:29:04 +0200

    ok

--- CreaList.php
+++ CreaList.php
@@ -1,23 +1,24 @@
 <?php
 include_once("db_info.php");
-
-if(isset($_POST["addElem"])){
-    $msg = " <p>Element : <input type=\"text\"  name=\"Elem1\" size=\"5\" /></p>";
-}
-else {
-    if (isset($_POST["NameElem"]) && isset($_POST["CtElem"])) {
-        $dbh = new PDO("$server:host=$host;dbname=$base", $user, $pass);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['addELem'])) {
+        $msg = "<p>Element : <input type=\"text\"  name=\"Elem1\" size=\"5\" /></p>";
+    } else {
+        echo("pas ok");
         if (isset($_POST["NameElem"]) && isset($_POST["CtElem"])) {
-            if ($_POST["NameElem"] != "" && $_POST["CtElem"] != "") {
-                $nam = $_POST["NameElem"];
-                $Cont = $_POST["CtElem"];
-                $sql = "INSERT INTO `LIST`(`nameList`) VALUES ('$nam') ";
-                $sql2 = "INSERT INTO `ELEMENT`(`TextElem`) VALUES ('$Cont')";
-                $dbh->exec($sql);
-                $dbh->exec($sql2);
-                header("Location: Connexion.php");
-            } else {
-                $msg = "erreur";
+            $dbh = new PDO("$server:host=$host;dbname=$base", $user, $pass);
+            if (isset($_POST["NameElem"]) && isset($_POST["CtElem"])) {
+                if ($_POST["NameElem"] != "" && $_POST["CtElem"] != "") {
+                    $nam = $_POST["NameElem"];
+                    $Cont = $_POST["CtElem"];
+                    $sql = "INSERT INTO `LIST`(`nameList`) VALUES ('$nam') ";
+                    $sql2 = "INSERT INTO `ELEMENT`(`TextElem`) VALUES ('$Cont')";
+                    $dbh->exec($sql);
+                    $dbh->exec($sql2);
+                    header("Location: Connexion.php");
+                } else {
+                    $msg = "erreur";
+                }
             }
         }
     }
@@ -31,10 +32,12 @@ else {
     <p>Inserer nom List <input type="text"  name="NameElem" size="5" /></p>
     <p>Element : <input type="text"  name="Elem1" size="5" /></p>
     <?php
-       echo($msg);
+    echo($msg);
     ?>
     <p><input type="submit",name="envoyer", value="ajouter"></p>
-    <p><input type="submit",name="addELem", value="ajouter element"></p>
+</form>
+<form method="post" action="CreaList.php">
+<p><input type="submit",name="addELem", value="ajouter element"></p>
 </form>
 </body>
 </html>

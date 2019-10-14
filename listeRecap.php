f2b7dfead198d506cc02644213e9e588637e9eea
SalomePechin <salome.pechin@gmail.com>
2019-10-14 15:56:21 +0200

    yo

--- listeRecap.php
+++ listeRecap.php
@@ -83,7 +83,7 @@ if (isset($_POST['nomPerson']) && isset($_POST['AjoutPerson'])){
         <div class="blocListe"></div>
     </div>
     <div class="footer">
-        <a href="http://la-myweb.univ-lemans.fr/~i180394/Connecte.php" class="bouton">Retour</a>
+        <a href="Connecte.php" class="bouton">Retour</a>
     </div>
 </div>
 </body>
